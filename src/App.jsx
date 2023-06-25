d988c6723a3697e8a128ed235f0663844cf64601
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-25 14:41:43 -0500

    imported a screen shot for the first project and finalized the layout of the work cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
      <Header />
      <Hero />
+     <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Projects.css'
+
+function Projects() {
+  return (
+    <section className="projects">
+      <h1>Projects</h1>
+      <div className="projects-container">
+        <div className='card'></div>
+        <div className='card'></div>
+      </div>
+      <div className="projects-container">
+        <div className='card'></div>
+        <div className='card'></div>
+      </div>
+    </section>
+  )
+}
+
+export default Projects
