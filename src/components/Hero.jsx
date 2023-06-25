fabe1bfb3cf31c1c3f25172264d1ef821486b530
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-25 13:36:50 -0500

    completed the visual layout for the banner section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero() {
+  return (
+    <section>
+      <div className="left-container">
+        <h1 className="hero-title">Shannon Brookshire</h1>
+        <h2 className="hero-subtitle">Web Developer</h2>
+        <p>Hi, I am a dedicated and enthusiastic web developer. 
+          I take pride in creating functional and preformant complex web applications.
+          Thank you for taking the time to review my portfolio.
+        </p>
+        <div id='resume-btn' style={{width: 180, height: 50, background: 'linear-gradient(270deg, #13ADC7 0%, #6978D1 67%, #945DD6 100%)', borderRadius: 83}}>RESUME</div>
+      </div>
+      <div className="right-container">
+        <div style={{width: 758, height: 758, position: 'relative', opacity: 0.50}}>
+          <div style={{width: 583.08, height: 583.08, left: 174.92, top: 0, position: 'absolute'}}>
+            <div style={{width: 495.62, height: 495.62, left: 87.46, top: 0, position: 'absolute'}}>
+              <div style={{width: 291.54, height: 291.54, left: 495.62, top: 495.62, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0'}}>
+                <div style={{width: 174.92, height: 174.92, left: 0, top: 0, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0'}}>
+                  <div style={{width: 87.46, height: 87.46, left: -87.46, top: 0, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: 0, top: -87.46, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px  solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: -174.92, top: -87.46, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: -87.46, top: 0, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                </div>
+                <div style={{width: 204.08, height: 204.08, left: -87.46, top: -87.46, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0'}}>
+                  <div style={{width: 116.62, height: 116.62, left: -87.46, top: -0, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 116.62, height: 116.62, left: -0, top: -87.46, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px  solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: -204.08, top: -116.62, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: -87.46, top: -0, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px  solid'}}></div>
+                </div>
+              </div>
+              <div style={{width: 291.54, height: 291.54, left: 0, top: 0, position: 'absolute'}}>
+                <div style={{width: 174.92, height: 174.92, left: 0, top: 0, position: 'absolute'}}>
+                  <div style={{width: 87.46, height: 87.46, left: 87.46, top: 0, position: 'absolute', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: 0, top: 87.46, position: 'absolute', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: 174.92, top: 87.46, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: 87.46, top: 0, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                </div>
+                <div style={{width: 204.08, height: 204.08, left: 87.46, top: 87.46, position: 'absolute'}}>
+                  <div style={{width: 116.62, height: 116.62, left: 87.46, top: 0, position: 'absolute', borderRadius: 83, border: '0.50px  solid'}}></div>
+                  <div style={{width: 116.62, height: 116.62, left: -0, top: 87.46, position: 'absolute', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: 87.46, top: 0, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                </div>
+              </div>
+            </div>
+            <div style={{width: 495.62, height: 495.62, left: 0, top: 87.46, position: 'absolute'}}>
+              <div style={{width: 291.54, height: 291.54, left: 204.08, top: 204.08, position: 'absolute'}}>
+                <div style={{width: 174.92, height: 174.92, left: 0, top: 0, position: 'absolute'}}>
+                  <div style={{width: 87.46, height: 87.46, left: 87.46, top: -0, position: 'absolute', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: -0, top: 87.46, position: 'absolute', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: 174.92, top: 87.46, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                </div>
+                <div style={{width: 204.08, height: 204.08, left: 87.46, top: 87.46, position: 'absolute'}}>
+                  <div style={{width: 116.62, height: 116.62, left: 87.46, top: 0, position: 'absolute', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 116.62, height: 116.62, left: 0, top: 87.46, position: 'absolute', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: 204.08, top: 116.62, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px  solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: 87.46, top: 0, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                </div>
+              </div>
+              <div style={{width: 291.54, height: 291.54, left: 291.54, top: 291.54, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0'}}>
+                <div style={{width: 174.92, height: 174.92, left: 0, top: 0, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0'}}>
+                  <div style={{width: 87.46, height: 87.46, left: -87.46, top: 0, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: -0, top: -87.46, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: -174.92, top: -87.46, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                </div>
+                <div style={{width: 204.08, height: 204.08, left: -87.46, top: -87.46, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0'}}>
+                  <div style={{width: 116.62, height: 116.62, left: -87.46, top: -0, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 116.62, height: 116.62, left: 0, top: -87.46, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: -204.08, top: -116.62, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px  solid'}}></div>
+                  <div style={{width: 87.46, height: 87.46, left: -87.46, top: -0, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+            <div style={{width: 583.08, height: 583.08, left: 0, top: 174.92, position: 'absolute'}}>
+              <div style={{width: 495.62, height: 495.62, left: 87.46, top: 0, position: 'absolute'}}>
+                <div style={{width: 291.54, height: 291.54, left: 495.62, top: 495.62, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0'}}>
+                  <div style={{width: 174.92, height: 174.92, left: 0, top: 0, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0'}}>
+                    <div style={{width: 87.46, height: 87.46, left: -87.46, top: 0, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: 0, top: -87.46, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: -174.92, top: -87.46, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: -87.46, top: 0, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  </div>
+                  <div style={{width: 204.08, height: 204.08, left: -87.46, top: -87.46, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0'}}>
+                    <div style={{width: 116.62, height: 116.62, left: -87.46, top: 0, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 116.62, height: 116.62, left: -0, top: -87.46, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: -204.08, top: -116.62, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: -87.46, top: 0, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px  solid'}}></div>
+                  </div>
+                </div>
+                <div style={{width: 291.54, height: 291.54, left: 0, top: 0, position: 'absolute'}}>
+                  <div style={{width: 174.92, height: 174.92, left: 0, top: 0, position: 'absolute'}}>
+                    <div style={{width: 87.46, height: 87.46, left: 87.46, top: 0, position: 'absolute', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: -0, top: 87.46, position: 'absolute', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: 174.92, top: 87.46, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: 87.46, top: 0, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  </div>
+                  <div style={{width: 204.08, height: 204.08, left: 87.46, top: 87.46, position: 'absolute'}}>
+                    <div style={{width: 116.62, height: 116.62, left: 87.46, top: 0, position: 'absolute', borderRadius: 83, border: '0.50px  solid'}}></div>
+                    <div style={{width: 116.62, height: 116.62, left: -0, top: 87.46, position: 'absolute', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: 87.46, top: 0, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  </div>
+                </div>
+              </div>
+              <div style={{width: 495.62, height: 495.62, left: -0, top: 87.46, position: 'absolute'}}>
+                <div style={{width: 291.54, height: 291.54, left: 204.08, top: 204.08, position: 'absolute'}}>
+                  <div style={{width: 174.92, height: 174.92, left: 0, top: 0, position: 'absolute'}}>
+                    <div style={{width: 87.46, height: 87.46, left: 87.46, top: 0, position: 'absolute', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: -0, top: 87.46, position: 'absolute', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: 174.92, top: 87.46, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  </div>
+                  <div style={{width: 204.08, height: 204.08, left: 87.46, top: 87.46, position: 'absolute'}}>
+                    <div style={{width: 116.62, height: 116.62, left: 87.46, top: 0, position: 'absolute', borderRadius: 83, border: '0.50px  solid'}}></div>
+                    <div style={{width: 116.62, height: 116.62, left: 0, top: 87.46, position: 'absolute', borderRadius: 83, border: '0.50px  solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: 204.08, top: 116.62, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: 87.46, top: 0, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  </div>
+                </div>
+                <div style={{width: 291.54, height: 291.54, left: 291.54, top: 291.54, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0'}}>
+                  <div style={{width: 174.92, height: 174.92, left: 0, top: 0, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0'}}>
+                    <div style={{width: 87.46, height: 87.46, left: -87.46, top: 0, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: 0, top: -87.46, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: -174.92, top: -87.46, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                  </div>
+                  <div style={{width: 204.08, height: 204.08, left: -87.46, top: -87.46, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0'}}>
+                    <div style={{width: 116.62, height: 116.62, left: -87.46, top: 0, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 116.62, height: 116.62, left: -0, top: -87.46, position: 'absolute', transform: 'rotate(-180deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: -204.08, top: -116.62, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px #505685 solid'}}></div>
+                    <div style={{width: 87.46, height: 87.46, left: -87.46, top: 0, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 83, border: '0.50px  solid'}}></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
