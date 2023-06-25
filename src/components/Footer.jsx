3ff1c2a1eed24531472f53f0394a06ae0e500e18
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-25 16:45:04 -0500

    completed the static layout of the site

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './Footer.css'
+import gitHub from '../assests/icons/mark-github-24.svg'
+
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="footer-container">
+      <p>Call Me: 1-719-419-0109</p>
+      <p>Email: <a href='#'>swbrookshire@gamil.com</a></p>
+      <div className="footer-icons">
+        <a href="#"><img src={gitHub} alt="git hub " /></a>
+        <a href="#"><img src={gitHub} alt="git hub " /></a>
+        <a href="#"><img src={gitHub} alt="git hub " /></a>
+      </div>
+    </div>
+)
 }
 
 export default Footer
