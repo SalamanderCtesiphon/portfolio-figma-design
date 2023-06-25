84d6a287de0bc1347b7b0d6af3714f6e8d14da4e
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-25 12:57:38 -0500

    completed the main layout for the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
+import logo from '../assests/icons/logo.svg';
+import gitHub from '../assests/icons/mark-github-24.svg';
+import linkdIn from '../assests/icons/Linkedin.svg';
+import figma from '../assests/icons/Figma.png'
 
 function Header() {
   return (
     <header>
-      <h1>Portfolio Header</h1>
+      <div className="header-container-1">
+        <img src={logo} alt="logo" id='logo'/>
+        <h2 className="header-title">Portfolio</h2>
+      </div>
+      <div className="header-container-2">
+        <a href="#" className="link"><h2>Projects</h2></a>
+        <a href="#" className="link"><h2>Technologies</h2></a>
+        <a href="#" className="link"><h2>About</h2></a>
+      </div>
+      <div className="header-container-3">
+        <a href="#" className="logo-link"><img src={gitHub} alt="gitHub" /></a>
+        <a href="#" className="logo-link"><img src={linkdIn} alt="linked in" /></a>
+        <a href="#" className="logo-link"><img src={figma} alt="figma" /></a>
+      </div>
     </header>
   )
 }
